94e4c704aa880b07ef7ce51a12831c205b9ac5ec
shanmugavelm-ops <shanmugavel_m@agnikul.in>
2026-06-23 12:19:30 +0530

    fix: sanitize secrets and clean k6 workflow

--- agk-perf-workspace/config/global_thresholds.js
+++
@@ -1,7 +0,0 @@
-export const globalThresholds = {
-  http_req_failed: ['rate<0.01'],
-  http_req_duration: ['p(95)<500', 'p(99)<1000'],
-  checks: ['rate>0.99'],
-};
-
-export default globalThresholds;
